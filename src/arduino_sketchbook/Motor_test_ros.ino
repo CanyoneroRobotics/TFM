992bd55dafcb7e2656c87741224b4dd059aa9448
oriolorra <oriolorraserra@gmail.com>
2016-05-25 13:22:09 +0200

    encoder update

--- src/arduino_sketchbook/Motor_test_ros.ino
+++ src/arduino_sketchbook/Motor_test_ros.ino
@@ -11,7 +11,6 @@
 #include <std_msgs/Empty.h>
 #include <std_msgs/Int32.h>
 
-
 Adafruit_MotorShield AFMS = Adafruit_MotorShield();
 Adafruit_DCMotor *myMotor1 = AFMS.getMotor(1);
 
@@ -23,8 +22,34 @@ void motor1VelCallBack(const std_msgs::Float32& msg){
   vel = msg.data;
 }
 void BWD1(){
-  encoder1_msg.data++;
+  // look for a low-to-high on channel A
+  if (digitalRead(19) == HIGH) { 
+    // check channel B to see which way encoder is turning
+    if (digitalRead(18) == HIGH) {  
+      encoder1_msg.data--;         // CW
+    } 
+    else {
+      //encoder1_msg.data--;         // CCW
+    }
+  }
+ 
 }
+
+void BWD2(){
+  // look for a low-to-high on channel A
+  if (digitalRead(18) == HIGH) { 
+    // check channel B to see which way encoder is turning
+    if (digitalRead(19) == HIGH) {  
+      encoder1_msg.data++;         // CW
+    } 
+    else {
+      //encoder1_msg.data--;         // CCW
+    }
+  }
+ 
+}
+
+
 
 ros::Subscriber<std_msgs::Float32> sub_vel1("/motor1_vel", &motor1VelCallBack);
 
@@ -41,7 +66,11 @@ void setup() {
   //Mega2560
   // external interrupt int.0    int.1    int.2   int.3   int.4   int.5            
   // pin                  2         3      21      20      19      18
+  pinMode(19, INPUT); 
+  pinMode(18, INPUT);
+  
   attachInterrupt(4, BWD1, CHANGE); // Also LOW, RISING, FALLING
+  attachInterrupt(5, BWD2, CHANGE); // Also LOW, RISING, FALLING
 
   AFMS.begin(); // create with the default frequency 1.6KHz
   //AFMS.begin(1000); // OR with a different frequency, say 1KHz
