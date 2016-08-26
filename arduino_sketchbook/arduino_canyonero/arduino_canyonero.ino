ba22f4a8a4d750e2ecc838b8f51bc83a5b899990
Juampe López <jplopez@circutor.com>
2016-08-26 23:10:39 +0200

    Roscontrol works!

--- arduino_sketchbook/arduino_canyonero/arduino_canyonero.ino
+++ arduino_sketchbook/arduino_canyonero/arduino_canyonero.ino
@@ -5,14 +5,14 @@
 #include <Adafruit_MotorShield.h>
 
 Adafruit_MotorShield AFMS = Adafruit_MotorShield();
-Adafruit_DCMotor *RL_motor = AFMS.getMotor(1);
-Adafruit_DCMotor *RR_motor = AFMS.getMotor(3);
+Adafruit_DCMotor *RL_motor = AFMS.getMotor(3);
+Adafruit_DCMotor *RR_motor = AFMS.getMotor(1);
 
 //ROS Variables
-uint16_t RL_speed;
-uint16_t RR_speed;
-uint16_t FL_speed;
-uint16_t FR_speed;
+int16_t RL_speed;
+int16_t RR_speed;
+int16_t FL_speed;
+int16_t FR_speed;
 
 int8_t RL_rads;
 int8_t RR_rads;
@@ -74,19 +74,24 @@ void writeToRoscontrol(void)
   delta_RL_ticks = RL_ticks - last_RL_ticks;
   delta_RR_ticks = RR_ticks - last_RR_ticks;
 
+  if (delta_RL_ticks < 0) delta_RL_ticks = abs(delta_RL_ticks);
+  if (delta_RR_ticks < 0) delta_RR_ticks = abs(delta_RR_ticks);
+
   RL_speed = 10 * ((delta_RL_ticks) * WHEEL_PERIM_CM) / (RL_TICKS * dt); // cm/ms to m/s
   RR_speed = 10 * ((delta_RR_ticks) * WHEEL_PERIM_CM) / (RR_TICKS * dt); // cm/ms to m/s
 
   if (RL_speed > 127) RL_speed = 127;
+  else if (RL_speed < -128) RL_speed = -128;
   if (RR_speed > 127) RR_speed = 127;
+  else if (RR_speed < -128) RR_speed = -128;
 
   // We don't have encoders for the front wheels so just reuse rear speeds
   FL_speed = RL_speed;
   FR_speed = RR_speed;
 
   last_time = millis();
-  last_RL_ticks = RL_speed;
-  last_RR_ticks = RR_speed;
+  last_RL_ticks = RL_ticks;
+  last_RR_ticks = RR_ticks;
 
   // Start of Packet
   Serial.write('{');
@@ -137,12 +142,19 @@ void readFromRoscontrol(void)
 
   if (c == EOP)
   {
-    digitalWrite(13, HIGH);
+    if (RL_rads > 0)
+    {
+      digitalWrite(13, HIGH);
+    }
+    else
+    {
+      digitalWrite(13, LOW);
+    }
 
     //RL_speed = (RL_rads * WHEEL_DIAM_CM / 2) / 100; // rad/s to cm/s to m/s
     //RR_speed = (RR_rads * WHEEL_DIAM_CM / 2) / 100; // rad/s to cm/s to m/s
 
-    if (FL_rads != 0)
+    if (RL_rads != 0)
       RL_speed = map(RL_rads, -10, 10, -255, 255);
     else
       RL_speed = 0;
@@ -161,24 +173,22 @@ void readFromRoscontrol(void)
     if (RL_speed >= 0)
     {
       RL_motor->setSpeed(RL_speed);
-      RL_motor->run(BACKWARD);
+      RL_motor->run(FORWARD);
     }
     else if (RL_speed < 0)
     {
       RL_motor->setSpeed(-RL_speed);
-      RL_motor->run(FORWARD);
+      RL_motor->run(BACKWARD);
     }
 
     if (RR_speed >= 0) {
       RR_motor->setSpeed(RR_speed);
-      RR_motor->run(BACKWARD);
+      RR_motor->run(FORWARD);
     }
     else if (RR_speed < 0) {
       RR_motor->setSpeed(-RR_speed);
-      RR_motor->run(FORWARD);
+      RR_motor->run(BACKWARD);
     }
-
-    digitalWrite(13, LOW);
   }
 }
 
