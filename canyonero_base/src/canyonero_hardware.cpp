d19433e55a5e9e70b3612c2a34619c7a155647e0
Juampe López <jplopez@circutor.com>
2016-08-26 18:56:49 +0200

    Debugging the messaging between ROS and Arduino.

--- canyonero_base/src/canyonero_hardware.cpp
+++ canyonero_base/src/canyonero_hardware.cpp
@@ -5,32 +5,32 @@
 
 int main(int argc, char **argv)
 {
-  ros::init(argc, argv, "canyonero_hardware_interface");
+        ros::init(argc, argv, "canyonero_hardware_interface");
 
-  ros::NodeHandle nh;
+        ros::NodeHandle nh;
 
-  Canyonero canyonero;
-  controller_manager::ControllerManager cm(&canyonero, nh);
+        Canyonero canyonero;
+        controller_manager::ControllerManager cm(&canyonero, nh);
 
-  ROS_INFO("Entering control loop");
+        ROS_INFO("Entering control loop");
 
-  //ros::CallbackQueue canyonero_queue;
-  ros::AsyncSpinner canyonero_spinner(1);
-  canyonero_spinner.start();
+        //ros::CallbackQueue canyonero_queue;
+        ros::AsyncSpinner canyonero_spinner(1);
+        canyonero_spinner.start();
 
-  // Control loop
-  while(ros::ok())
-  {
-      ROS_INFO_ONCE("Control loop: Hi!");
-    canyonero.read();
-    cm.update(canyonero.get_time(), canyonero.get_period());
-    canyonero.write();
-    usleep(100000);
+        // Control loop
+        while(ros::ok())
+        {
+                ROS_INFO_ONCE("Control loop: Hi!");
+                canyonero.read();
+                cm.update(canyonero.get_time(), canyonero.get_period());
+                canyonero.write();
+                usleep(100000);
 
-    //ros::spinOnce();
-  }
+                //ros::spinOnce();
+        }
 
-  ROS_INFO("We exited the control loop");
+        ROS_INFO("We exited the control loop");
 
-  return 0;
+        return 0;
 }
